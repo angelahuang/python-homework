802d285cf7450a84b0a164bbd66b2b9d781646c8
angelahuang <angelahuang@kkbox.com>
2015-02-03 14:19:19 +0800

    中文九九乘法表（加上拾）_v2

---
+++ 20150203.py
@@ -0,0 +1,34 @@
+#-*- coding: utf-8 -*-
+num = ['零','壹','貳','叄','肆','伍','陸','柒','捌','玖'] 
+string = ['乘','等於','拾']
+for i in range(1,10):
+	for j in range(1,10):
+		print num[i],string[0],num[j],string[1],
+		lenAA = len(str(i*j))
+		temp = 0
+		for S in str(i*j):
+			print num[int(S)],
+			temp += 1
+			if temp != lenAA:
+				print string[2],
+		print
+	print '\n'
+	
+	
+	
+	
+	#============================== Knet 做法 ================================
+num = ['零','壹','貳','叄','肆','伍','陸','柒','捌','玖'] 
+string = ['乘','等於','拾']
+for i in range(1,10):
+	for j in range(1,10):
+		print num[i],string[0],num[j],string[1],
+		AA = str(i*j)
+		if i*j >= 10:
+			print num[int(AA[0])],
+			print string[2],
+			print num[int(AA[1])],
+		else:
+			print num[int(AA[0])],
+		print
+	print '\n'
